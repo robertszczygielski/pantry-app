e6df62d3c056c2a947812a4f25419c4a1df7e57b
Robert Szczygielski <robertszczygielski@wp.eu>
2020-06-07 21:02:37 +0200

    add translations

--- src/components/BasicForm/index.js
+++ src/components/BasicForm/index.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { Field, Form, Formik } from "formik";
 import { addProduct, getProducts, updateProduct } from "../../restapi/BeckEnd/Product";
 import { ShowProducts } from "./ShowProducts";
+import { DangerMessage } from "./DangerMessage";
 
 export class BasicForm extends Component {
     state = {
         product: null,
-        nameError: ''
+        nameError: '',
+        amountError: ''
     }
 
     componentDidMount() {
@@ -15,10 +17,18 @@ export class BasicForm extends Component {
         }).catch();
     }
 
+    errorMapper = errorCode => {
+        if (errorCode === 'PANTRY_PRODUCT_NAME_NO_SMALL_LETTER') {
+            this.setState({ nameError: `error.message.${errorCode}`});
+        } else if (errorCode === 'PANTRY_PRODUCT_AMOUNT_GREATER_THAN_ZERO') {
+            this.setState({ amountError: `error.message.${errorCode}` });
+        }
+    }
+
     onSubmit = async value => {
         const resp = await addProduct(value);
         if (resp.status === 500) {
-            this.setState({nameError: resp.data.message});
+            this.errorMapper(resp.data.message);
         }
 
         getProducts().then( response  => {
@@ -49,9 +59,10 @@ export class BasicForm extends Component {
                                 <Form>
                                     <label htmlFor="name">Name</label>
                                     <Field className="form-control" type="text" name="name"/>
-                                    {this.state.nameError && <p className="alert alert-danger">{this.state.nameError}</p>}
+                                    {this.state.nameError && <DangerMessage message={this.state.amountError} />}
                                     <label htmlFor="name">Amount</label>
                                     <Field className="form-control" type="text" name="amount"/>
+                                    {this.state.amountError && <DangerMessage message={this.state.amountError} />}
                                     <button className="btn" type="submit">
                                         Send
                                     </button>

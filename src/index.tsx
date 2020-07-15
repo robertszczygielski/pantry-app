6e33a6516a2bb02f886cea3d5cf9457bf0a02e4f
szczygr <Robert.Szczygielski@qiagen.com>
2020-07-15 09:16:25 +0200

    Add Typescript dependencies

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './index.css';
+import './local/i18n'
+
+ReactDOM.render(
+    <h1>Hello</h1>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+    document.getElementById('root') as HTMLElement
+);
